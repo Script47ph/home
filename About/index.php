6e09857274d9e85ea8d745cb1da4a20e5aa1d799
Script47ph <script47ind@gmail.com>
2019-08-27 17:44:13 +0000

    About

---
+++ About/index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    sleep(2);
+    ?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Tentang Kami ~ Pustaka Anime</title>
+     <link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/all.css" />
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <link rel="shortcut icon" href="Img/default2.png">
+
+        <!-- STYLES -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <!-- FONTS -->
+        <link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono">
+
+        <!-- META -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <style type="text/css">
+        #notfound .notfound-bg:after{
+            background-color: #222;
+        }
+        .notfound .rilisan-btn:hover, .notfound .about-btn:hover, .notfound .list-btn:hover, .notfound-social>a:hover{
+        color: rgba(29, 24, 25, 0.7)
+        }
+    </style>
+</head>
+
+<body>
+    <div id="notfound">
+        <div class="notfound-bg"></div>
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1 style="font-size: 30px;">Tentang Kami</h1>
+            </div>
+            <h2 style="font-size: 16px;">Ini adalah Web download anime bd dengan segala resolusi dan format yang kalian sendiri bebas memilihnya :D. Jika ada pertanyaan atau kalian ingin request anime yang mau di garap bd nya, bisa chat ke fp aja ya (Palingan ga bakal digarap sama miminnya) :v</h2>
+            <a href="/" class="about-btn">Home</a>
+            <a href="/Daftar%20Anime/" class="list-btn">Daftar Anime</a>
+            <a href="/Rilisan%20Terbaru/" class="rilisan-btn">Rilisan Terbaru</a>
+            
+            
+            <div class="notfound-social">
+                <a href="https://www.facebook.com/pustakawibu/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-pinterest"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-discord"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-telegram"></i></a>
+            </div>
+        </div>
+    </div>
+
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body>
+
+</html>
